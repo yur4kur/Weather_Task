84c24a11b2105050a66de6ff69801d8c4f915e6c
Yuri Kurinnoi <yur4kur@icloud.com>
2022-08-04 11:42:37 +0400

    Initial Commit

---
+++ Weather_Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather_Task
+//
+//  Created by Пользователь on 04/08/2022.
+//  Copyright © 2022 Пользователь. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
